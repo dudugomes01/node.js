0d6c459417d607eab9298621a23fdbf08f146cbe
Carlos Eduardo Gomes <85941223+dudugomes01@users.noreply.github.com>
2022-08-09 15:02:09 -0300

    modulo 3

---
+++ aula3/calculos.js
@@ -0,0 +1,12 @@
+function soma(n1, n2){
+    return n1 + n2
+}
+
+function multiplicar(n1, n2){
+    return n1* n2
+}
+
+module.exports = {
+    soma,
+    multiplicar
+}
---
+++ aula3/index.js
@@ -0,0 +1,4 @@
+const {soma, multiplicar} = require('./calculos.js')
+
+const resultado = multiplicar(10, 20)
+console.log(resultado)
