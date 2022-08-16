1e000bb7c8ad2bc2c275ba78908c57aafd0fe5e1
Carlos Eduardo <eduardogomes114@yahoo.com>
2022-08-16 10:52:50 -0300

    adicionando

---
+++ aula3/index.js
@@ -0,0 +1,8 @@
+const {soma, multiplicar} = require('./calculos.js')
+
+const resultado = multiplicar(10, 20)
+
+const somar = soma(10, 20)
+
+console.log(somar)
+console.log(resultado)
---
+++ aula4/index.js
@@ -0,0 +1,10 @@
+const fs = require("fs")
+
+fs.readFile('./clientes.json', function(error, content){
+    if(error){
+        console.log("Ops, deu erro!")
+    }
+    else{
+        console.log(JSON.parse(content))
+    }
+})
